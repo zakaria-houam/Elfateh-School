5d4fbaeabb3602479f7c8408150ecf67a67756a6
zakaria-houam <zakariahouamzakaria@gmail.com>
2021-10-22 19:59:05 +0100

    nami

--- files pages/java.js
+++ files pages/java.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $("#color-range").on("input change", function(){
         $("header").css("background-color",$(this).val());
         $(".pdf a").css("background",$(this).val())
-        $("footer h1").css("color",$(this).val());
+        $("footer").css("background",$(this).val());
 
         console.log("hello")
     })
